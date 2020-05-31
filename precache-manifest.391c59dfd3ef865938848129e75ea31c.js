9e25b3e4c2036b5df57c03e263c4e26f87388c79
juan camilo acevedo <juancaacevedo10@hotmail.com>
2020-05-31 12:17:46 -0500

    Updates

---
+++ precache-manifest.391c59dfd3ef865938848129e75ea31c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6193ac95b17111c169984258f45b4d42",
+    "url": "/Random-Quote-Machine/index.html"
+  },
+  {
+    "revision": "7afcc5956e3e178126b2",
+    "url": "/Random-Quote-Machine/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "329d6f79091f1d2c5f56",
+    "url": "/Random-Quote-Machine/static/css/main.6663c8b8.chunk.css"
+  },
+  {
+    "revision": "7afcc5956e3e178126b2",
+    "url": "/Random-Quote-Machine/static/js/2.f6e99cf7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Random-Quote-Machine/static/js/2.f6e99cf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "329d6f79091f1d2c5f56",
+    "url": "/Random-Quote-Machine/static/js/main.c6266364.chunk.js"
+  },
+  {
+    "revision": "90b6a4bafaaf17be1ec6",
+    "url": "/Random-Quote-Machine/static/js/runtime-main.cae3487c.js"
+  }
+]);
